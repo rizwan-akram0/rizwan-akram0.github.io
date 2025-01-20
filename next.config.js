2d08c3d853b69235c2ff8751324ac7d65b0d0418
Rizwan Akram <rizwanakramyt@gmail.com>
2025-01-21 01:56:04 +0500

    Portfolio complete

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
+  output: "export",
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -23,4 +26,4 @@ module.exports = {
       },
     ],
   },
-}
+};
